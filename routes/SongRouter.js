6f6c11aae6b9bf102632adaf7398ee6608a6cf79
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-05-10 13:33:11 -0400

    made routes for songs

--- routes/SongRouter.js
+++ routes/SongRouter.js
@@ -1,4 +1,7 @@
 const Router = require('express').Router()
 const controller = require('../controllers/SongController')
 
+Router.get('/', controller.getSongs)
+Router.post('/', controller.createSongs)
+
 module.exports = Router
