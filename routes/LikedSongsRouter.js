46d96b24e965c7bad30fecf7f0356eeeb0b9a64b
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-05-10 11:44:40 -0400

    created home route for likedsongs

--- routes/LikedSongsRouter.js
+++ routes/LikedSongsRouter.js
@@ -1,4 +1,8 @@
 const Router = require('express').Router()
 const controller = require('../controllers/LikedSongsController')
 
+Router.get('/', (req, res) => {
+  res.send('hello')
+})
+
 module.exports = Router
