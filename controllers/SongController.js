d6c93d29dd78a3308d1ee2a046d66cb197fff27e
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-05-10 13:32:31 -0400

    made create songs controller

--- controllers/SongController.js
+++ controllers/SongController.js
@@ -9,6 +9,23 @@ const getSongs = async (req, res) => {
   }
 }
 
+const createSongs = async (req, res) => {
+  try {
+    let artistId = parseInt(req.params.artist_id)
+    let albumId = parseInt(req.params.ablum_id)
+    let songBody = {
+      artistId,
+      albumId,
+      ...req.body
+    }
+    let results = await Song.create(songBody)
+    res.send(results)
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
-  getSongs
+  getSongs,
+  createSongs
 }
