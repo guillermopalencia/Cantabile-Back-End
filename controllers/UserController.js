365f2336a61e1dbd4173c58a1b61f37279363103
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-05-10 13:08:49 -0400

    created geuseplaylist controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,6 +10,23 @@ const getUsers = async (req, res) => {
   }
 }
 
+const getUserPlayList = async (req, res) => {
+  try {
+    const list = await User.findAll({
+      include: [
+        {
+          model: Song,
+          as: 'play_list',
+          through: { attributes: [] }
+        }
+      ]
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
-  getUsers
+  getUsers,
+  getUserPlayList
 }
