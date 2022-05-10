5a94c4d9e4eca581f46284aae9e6b17ecf3157c4
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-05-10 13:16:45 -0400

    made createuser route

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/UserController')
 Router.get('/', controller.getUsers)
 Router.get('/playlist', controller.getUserPlayList)
 Router.get('/likedsongs', controller.getUserLikedSongs)
+Router.post('/', controller.createUser)
 
 module.exports = Router
